c8a185c0ef42488bf883ff346632917b6ad3c761
tyoffadhil08 <tyoffadhilhaidar@gmail.com>
2023-11-21 16:29:17 +0700

    Tyoffadhil Haidar Ismail

---
+++ StudentSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        //
+        $data = [];
+            for ($i = 3; $i <50; $i++) {
+                // ['id' => , 'name' => $faker->name(), 'score' =>80],
+                // ['id' => 2, 'name' => 'Tyoffadhil', 'score' =>80],
+                // ['id' => 2, 'name' => 'Haidar', 'score' =>90],
+                // ['id' => 3, 'name' => 'Retno', 'score' =>100],
+                $data[] = [
+                    'id' => $i + 1,
+                    'name' => $faker->name(),
+                    'score' => $faker->numberBetween(50, 100),
+                ];
+            }
+            DB::table('student')->insert($data);
+    }
+}
